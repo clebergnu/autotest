b2b0af9a829aa15116a6632c6c845448a38bb63a
Cleber Rosa <crosa@redhat.com>
2013-05-21 15:20:31 -0300

    backports: move tests on namedtuple to unittest file
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- client/shared/backports_unittest.py
+++ client/shared/backports_unittest.py
@@ -20,11 +20,15 @@
 
 import unittest
 import sys
+import pickle
+import doctest
+
 try:
     import autotest.common as common
 except ImportError:
     import common
 from autotest.client.shared import backports
+from autotest.client.shared.backports.collections import namedtuple
 
 
 def is_python_26_or_greater():
@@ -33,6 +37,11 @@ def is_python_26_or_greater():
     if sys.version_info[0] >= 2 and sys.version_info[1] >= 6:
         return True
     return False
+
+
+# Used in namedtuple unittests, put at module scope to cope with pickle:
+# "classes that are defined at the top level of a module"
+Point = namedtuple('Point', 'x, y')
 
 
 class TestBackports(unittest.TestCase):
@@ -71,6 +80,51 @@ class TestBackports(unittest.TestCase):
             assert False
         except TypeError:
             pass
+
+
+    def test_namedtuple_pickle(self):
+        '''
+        Verify that instances can be pickled
+        '''
+        p = Point(x=10, y=20)
+        self.assertEquals(p, pickle.loads(pickle.dumps(p, -1)))
+
+
+    def test_namedtuple_override(self):
+        '''
+        Test and demonstrate ability to override methods
+        '''
+        class HypotPoint(namedtuple('Point', 'x y')):
+            @property
+            def hypot(self):
+                return (self.x ** 2 + self.y ** 2) ** 0.5
+
+            def __str__(self):
+                return 'Point: x=%6.3f y=%6.3f hypot=%6.3f' % (self.x,
+                                                               self.y,
+                                                               self.hypot)
+        p1 = HypotPoint(3, 4)
+        p2 = HypotPoint(14, 5)
+        p3 = HypotPoint(9. / 7, 6)
+        self.assertEquals(p1.hypot, 5.0)
+        self.assertAlmostEquals(p2.hypot, 14.866068747318506)
+        self.assertAlmostEquals(p3.hypot, 6.136209027118437)
+
+
+    def test_namedtuple_optimize(self):
+        """
+        Tests a point class with optimized _make() and _replace()
+
+        The optimized versions have no error-checking
+        """
+        class OptimizedPoint(namedtuple('Point', 'x y')):
+            _make = classmethod(tuple.__new__)
+
+            def _replace(self, _map=map, **kwds):
+                return self._make(_map(kwds.get, ('x', 'y'), self))
+
+        p = OptimizedPoint(11, 22)._replace(x=100)
+        self.assertEquals(p.x, 100)
 
 
 if __name__ == '__main__':
